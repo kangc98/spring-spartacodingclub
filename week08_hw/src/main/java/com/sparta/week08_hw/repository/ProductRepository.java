9cfe17e0c4897e6de580d7b755328a69d4286a81
kksswkd <kksswkd@gmail.com>
2021-11-02 02:51:38 +0900

    sparta week08 hw

---
+++ week08_hw/src/main/java/com/sparta/week08_hw/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.week08_hw.repository;
+
+import com.sparta.week08_hw.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findAllByUserId(Long userId);
+}
