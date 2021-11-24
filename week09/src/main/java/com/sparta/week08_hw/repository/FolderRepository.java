d128221c3c6c29b6277deb0be08bd63861bb55c0
kksswkd <kksswkd@gmail.com>
2021-11-25 01:43:23 +0900

    sparta week09

---
+++ week09/src/main/java/com/sparta/week08_hw/repository/FolderRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.week08_hw.repository;
+
+import com.sparta.week08_hw.model.Folder;
+import com.sparta.week08_hw.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FolderRepository extends JpaRepository<Folder, Long> {
+    List<Folder> findAllByUser(User user);
+}
